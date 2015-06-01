324a982dd5aac2cf0f9dbeb6d01b88be7aef869e
Alex Ley <avade@pivotal.io>
2015-06-01 17:31:25 +0100

    Support atrifactory license file in BOSH manifest
    Signed-off-by: Jonathan Roquelaure <jonathanr@jfrog.com>

--- README.md
+++ README.md
@@ -17,3 +17,46 @@ then determine the appropriate bosh used for development and run
 ```
 bosh target <insert bosh location here>
 ```
+=======
+#Artifactory BOSH release
+
+## Configuration
+
+- To use this release you must provide the path to an Artifactory license file
+in an environment variable called ARTIFACTORY_LICENSE_PATH.
+
+`ARTIFACTORY_LICENSE_PATH=... bosh deploy`
+
+## Running the tests
+
+Note that the tests expect artifactory to have been deployed to your BOSH
+environment. You must specify two license keys with different valid through dates
+to use during testing.
+
+### Via Vagrant
+
+```
+export 	BOSH_TARGET=192.168.50.4
+        BOSH_DIRECTOR_SSH_USERNAME=vagrant
+        BOSH_DIRECTOR_SSH_PASSWORD=vagrant
+        BOSH_MANIFEST=manifests/artifactory-lite.yml
+        TEST_LICENSE_1=...
+        TEST_LICENSE_2=...
+bundle exec rspec --format d
+
+```
+
+### Via vSphere
+
+```
+export BOSH_TARGET=*vshpere bosh director IP*
+	   BOSH_USERNAME=*vshpere bosh admin username*
+	   BOSH_PASSWORD=*vshpere bosh admin password*
+       BOSH_DIRECTOR_SSH_USERNAME=*vshpere bosh director ssh username*
+       BOSH_DIRECTOR_SSH_PASSWORD=*vshpere bosh director ssh password*
+       BOSH_MANIFEST=manifests/artifactory-vsphere.yml
+       TEST_LICENSE_1=...
+       TEST_LICENSE_2=...
+bundle exec rspec --format d
+
+```
--- spec/standalone_spec.rb
+++ spec/standalone_spec.rb
@@ -4,21 +4,66 @@ require 'rest-client'
 require 'yaml'
 
 describe 'Standalone Artifactory' do
-  it 'should verify that deployed artifactory is running and version is correct' do
+
+  before(:all) do
+    @gateway = Net::SSH::Gateway.new(bosh_target, bosh_director_ssh_username,
+                                      :password => bosh_director_ssh_password)
+    @standalone_node_ip = get_standalone_node_ip_from bosh_manifest
     bundle_exec_bosh "target #{bosh_target}"
     bundle_exec_bosh "deployment #{bosh_manifest}"
     bundle_exec_bosh "login #{bosh_username} #{bosh_password}"
 
-    gateway = Net::SSH::Gateway.new(bosh_target, bosh_director_ssh_username,
-                                    :password => bosh_director_ssh_password)
+    ENV["ARTIFACTORY_LICENSE_PATH"] = ENV["TEST_LICENSE_1"]
+    bosh_deploy_and_wait_for_artifactory
+  end
 
-    standalone_node_ip = get_standalone_node_ip_from bosh_manifest
-
-    gateway.open(standalone_node_ip, artifactory_port) do |port|
+  it 'should verify that deployed artifactory is running and version is correct' do
+    exec_on_gateway do | port |
       response = RestClient.get artifactory_version_url port
       expect(JSON.parse(response)['version']).to eq(expected_artifactory_version)
     end
   end
+
+  describe 'licensing' do
+    it 'should have a license present' do
+      exec_on_gateway do | port |
+        response = RestClient.get artifactory_license_url port
+        expect(JSON.parse(response)['type']).to eq('Trial')
+      end
+    end
+
+    context 'when license is changed' do
+
+      #update and deploy the new manifest
+      before(:all) do
+        exec_on_gateway do | port |
+          response = RestClient.get artifactory_license_url port
+          $original_expiry_date = JSON.parse(response)['validThrough']
+        end
+        ENV["ARTIFACTORY_LICENSE_PATH"] = ENV["TEST_LICENSE_2"]
+        bosh_deploy_and_wait_for_artifactory
+      end
+
+      #reset the BOSH deployment to the original
+      after(:all) do
+        ENV["ARTIFACTORY_LICENSE_PATH"] = ENV["TEST_LICENSE_1"]
+        bundle_exec_bosh "-n deploy"
+      end
+
+      it 'updates the license' do
+        exec_on_gateway do | port |
+            response = RestClient.get artifactory_license_url port
+            expect(JSON.parse(response)['validThrough']).to_not eq(@original_expiry_date)
+        end
+      end
+
+    end
+  end
+end
+
+def bosh_deploy_and_wait_for_artifactory
+  bundle_exec_bosh "deploy"
+  wait_for_artifactory_available
 end
 
 def bundle_exec_bosh command
@@ -30,6 +75,27 @@ def get_standalone_node_ip_from bosh_manifest
  YAML.load_file(bosh_manifest)['jobs'].
    find{|job_hash| job_hash['name'] == 'standalone'}['networks'].
    first['static_ips'].first
+end
+
+def exec_on_gateway
+  @gateway.open(@standalone_node_ip, artifactory_port) do |port|
+    yield port
+  end
+end
+
+def wait_for_artifactory_available
+  exec_on_gateway do | port |
+    begin
+      RestClient.get artifactory_status_api port
+    rescue RestClient::ServiceUnavailable
+      puts "still waiting for artifactory to become available, sleeping for 5secs"
+      sleep(5)
+      retry
+    rescue RestClient::Forbidden
+      puts "license maybe expired"
+    end
+    puts "artifactory available"
+  end
 end
 
 def bosh_target
@@ -52,12 +118,36 @@ def expected_artifactory_version
   ENV['EXPECTED_ARTIFACTORY_VERSION'] || '3.7.0'
 end
 
+def artifactory_admin_user
+  "admin"
+end
+
+def artifactory_admin_password
+  "password"
+end
+
 def artifactory_port
   ENV['ARTIFACTORY_PORT'] || 8081
 end
 
+def artifactory_api port
+  "http://localhost:#{port}/artifactory/api"
+end
+
+def artifactory_authenticated_api port
+  "http://#{artifactory_admin_user}:#{artifactory_admin_password}@localhost:#{port}/artifactory/api"
+end
+
+def artifactory_status_api port
+ "#{artifactory_authenticated_api(port)}/system/ping"
+end
+
 def artifactory_version_url port
- "http://localhost:#{port}/artifactory/api/system/version"
+ "#{artifactory_api(port)}/system/version"
+end
+
+def artifactory_license_url port
+ "#{artifactory_authenticated_api(port)}/system/license"
 end
 
 def bosh_director_ssh_username
